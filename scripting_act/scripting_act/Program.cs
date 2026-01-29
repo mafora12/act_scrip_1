e0017c0117db2c6c105e97ebb9888f8ee48fd722
mafora12 <marianaflorez1224@gmail.com>
2026-01-28 20:41:18 -0500

    actualizacion

--- scripting_act/scripting_act/Program.cs
+++ scripting_act/scripting_act/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static string SegundosAHora(int totalSegundos)
+{
+    int horas = totalSegundos / 3600;
+    int minutos = (totalSegundos % 3600) / 60;
+    int segundos = totalSegundos % 60;
+
+    return $"{horas:D2}:{minutos:D2}:{segundos:D2}";
+}
